f78ff050837411477ae906695cd645769f86d1b6
peterbenoit <peterbenoit@gmail.com>
2026-01-09 10:01:16 -0500

    Remove config.js from repo and add to gitignore

---
+++ config.example.js
@@ -0,0 +1,10 @@
+// config.example.js
+// IMPORTANT: Copy this file to config.js and add YOUR OWN GitHub OAuth credentials
+// DO NOT use someone else's credentials - create your own OAuth app!
+
+const CONFIG = {
+	client_id: 'YOUR_CLIENT_ID_HERE',
+	client_secret: 'YOUR_CLIENT_SECRET_HERE'
+};
+
+export default CONFIG;
--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-
-const CONFIG = {
-    client_id: 'YOUR_CLIENT_ID',
-    client_secret: 'YOUR_CLIENT_SECRET'
-};
-
-// Export the CONFIG object
-export default CONFIG;
